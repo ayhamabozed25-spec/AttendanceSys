8c6090da5717ba0afd5d9e37b3c91ca959b2cb39
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-16 10:58:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL="https://script.google.com/macros/s/AKfycbw_S7hc3Jl1dVBdEsmOtkbyAdH7u_nreRTFHsWgQ54vmpaSHtuBFlBu66p134xnJmX-/exec"
+const API_URL="https://script.google.com/macros/s/AKfycbxOkiCgebtxpmvXjs1KsWquh9UJ7eHonTqT6zP7nfzfqsMULXMMKGNVLMf1cWSnbF5V/exec"
